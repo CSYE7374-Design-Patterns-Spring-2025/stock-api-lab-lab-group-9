0fc4b9528ce917dc9a2e03f2cf5578f8ce71a3d6
NilrajMayekar <mayekar.n@northeastern.edu>
2025-01-21 15:48:09 -0500

    3 more Stock class implementation

--- src/main/java/edu/neu/csye7374/StockMarket.java
+++ src/main/java/edu/neu/csye7374/StockMarket.java
@@ -61,6 +61,27 @@ public class StockMarket {
         cgi.setBid("7.0");
         stock.add(cgi);
 
+        Stock amazon = new Amazon("Amazon", 50.0, "amazon  Description", new ArrayList<>(Arrays.asList(40.0, 42.0, 45.0, 48.0, 49.5, 50.0)));
+        amazon.setBid("52.0");
+        amazon.setBid("53.5");
+        amazon.setBid("54.0");
+        amazon.setBid("55.0");
+        stock.add(amazon);
+
+        Stock tcs = new Tcs("Tcs", 70.0, "Tcs  Description", new ArrayList<>(Arrays.asList(65.0, 67.0, 68.0, 69.0, 70.0, 72.0)));
+        tcs.setBid("74.0");
+        tcs.setBid("76.0");
+        tcs.setBid("78.0");
+        tcs.setBid("80.0");
+        stock.add(tcs);
+
+        Stock walmart = new Walmart("Walmart", 30.0, "Walmart  Description", new ArrayList<>(Arrays.asList(25.0, 27.0, 28.0, 29.5, 30.0, 32.0)));
+        walmart.setBid("33.0");
+        walmart.setBid("35.0");
+        walmart.setBid("36.0");
+        walmart.setBid("37.0");
+        stock.add(walmart);
+
         StockMarket stockMarket = StockMarket.getInstance();
         stockMarket.addAllStocks(stock);
         System.out.println("-----------------Initial Market--------------------");
@@ -68,14 +89,29 @@ public class StockMarket {
 
         stockMarket.tradeStock("Accenture", 10.0);
         stockMarket.tradeStock("Cgi", 19.0);
+        stockMarket.tradeStock("Amazon", 55.0);
+        stockMarket.tradeStock("Walmart", 36.0);
+        stockMarket.tradeStock("TCS", 80.0);
 
         System.out.println("-----------------Market after one bid--------------------");
         System.out.println(stockMarket);
 
         stockMarket.tradeStock("Accenture", 9.0);
         stockMarket.tradeStock("Cgi", 11.0);
+        stockMarket.tradeStock("Amazon", 58.0);
+        stockMarket.tradeStock("Walmart", 38.0);
+        stockMarket.tradeStock("TCS", 85.0);
 
         System.out.println("-----------------Market after two bids--------------------");
+        System.out.println(stockMarket);
+
+        stockMarket.tradeStock("Accenture", 12.0);
+        stockMarket.tradeStock("Cgi", 22.0);
+        stockMarket.tradeStock("Amazon", 60.0);
+        stockMarket.tradeStock("Walmart", 40.0);
+        stockMarket.tradeStock("TCS", 90.0);
+
+        System.out.println("-----------------Market after three bids--------------------");
         System.out.println(stockMarket);
 
         stockMarket.deleteStock(accenture);
---
+++ src/main/java/edu/neu/csye7374/Walmart.java
@@ -0,0 +1,29 @@
+package edu.neu.csye7374;
+
+import java.util.List;
+
+public class Walmart extends Stock{
+    public Walmart(String name, double price, String description, List<Double> prevPrc){
+        super();
+        setName(name);
+        setPrice(price);
+        setDescription(description);
+        setPrevPrc(prevPrc);
+    }
+
+    @Override
+    public String getMetric() {
+        int perf = 0;
+        double prevPrc = 0;
+        for (double price : getPrevPrc().subList(getPrevPrc().size() - 6, getPrevPrc().size())
+        ) {
+            if (price > prevPrc) {
+                perf++;
+            } else {
+                perf--;
+            }
+            prevPrc = price;
+        }
+        return String.valueOf(perf);
+    }
+}
